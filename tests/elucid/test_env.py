32a116a7f0d3529439b7c5586cf6025423ec29ee
Yangyao Chen <yangyaochen.astro@foxmail.com>
2025-11-22 14:11:01 +0800

    remove mandatory PYTHONPATH

--- tests/elucid/test_env.py
+++ tests/elucid/test_env.py
@@ -1,7 +1,7 @@
 import os, elucid
 
 def test_env():
-    print('Python PATH:', os.environ['PYTHONPATH'])    
+    print('Python PATH:', os.environ.get('PYTHONPATH', None))    
     
 def test_version():
     print(elucid.version)
